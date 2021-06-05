e6f9cbaa14a727338a14002d3e9e6a035ee35a0b
Catherine Loesch <catherine.loesch@protonmail.com>
2021-06-05 02:19:58 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+
+function findMatching(arr, str){
+   const filteredArray = arr.filter(function(element){
+      return element.toLowerCase() === str.toLowerCase()
+   })
+   return filteredArray
+}
+
+function fuzzyMatch(arr, str){
+    const length = str.length
+    const filteredArray = arr.filter(function(element){
+       return element.toLowerCase().slice(0, length) === str.toLowerCase().slice(0, length)
+    })
+    return filteredArray
+ }
+
+function matchName(arr, str){
+    const filteredArray = arr.filter(function(element){
+        return element.name.toLowerCase() === str.toLowerCase()
+     })
+     return filteredArray
+}
+
